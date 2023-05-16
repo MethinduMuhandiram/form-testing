b5eb5b5487ad7c02e560790079120e7c4fd8fd36
MethinduMuhandiram <methindupm@gmail.com>
2023-05-16 08:30:25 +0530

    updated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,138 +1,174 @@
 import React, { useState } from "react"
+import { Form, Button, Row, Col } from "react-bootstrap"
 import { navigate } from "gatsby-link"
-import { TextField, Button } from "@mui/material"
-import Switch from "@mui/material/Switch"
-import SocialMediaLinks from "../components/socialMediaLinks"
-import Subscribe from "../components/subscribe"
 
 // Form Helpers
+// function encode(data) {
+//   return Object.keys(data)
+//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//     .join("&")
+// }
+
 function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&")
+  const formData = new FormData()
+
+  Object.keys(data).forEach(key => {
+    if (Array.isArray(data[key])) {
+      data[key].forEach(value => {
+        formData.append(key + "[]", value)
+      })
+    } else {
+      formData.append(key, data[key])
+    }
+  })
+
+  return new URLSearchParams(formData).toString()
 }
+
 const ContactForm = () => {
-  const [checked, setChecked] = useState(true)
+  const checkboxList = [
+    "Website",
+    "Web Application",
+    "Mobile App",
+    "Social Media",
+    "Design",
+    "SEO",
+    "Other",
+  ]
 
-  const switchHandler = event => {
-    setChecked(event.target.checked)
-  }
-
-  const [fullName, setfullName] = useState("")
-  const [company, setCompany] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
   const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [checkedItems, setCheckedItems] = useState([])
   const [message, setMessage] = useState("")
 
-  const handleSubmit = event => {
-    event.preventDefault()
+  const handleCheckboxChange = event => {
+    const itemName = event.target.name
 
-    const contactForm = event.target
-    const formData = new FormData(contactForm)
+    setCheckedItems(prevCheckedItems => {
+      if (prevCheckedItems.includes(itemName)) {
+        return prevCheckedItems.filter(item => item !== itemName)
+      } else {
+        return [...prevCheckedItems, itemName]
+      }
+    })
+  }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+      body: encode({
+        "form-name": "Contact",
+        firstName,
+        lastName,
+        email,
+        phone,
+        message,
+        services: checkedItems,
+      }),
     })
-      .then(() => navigate("/"))
-      .catch(error => alert(error))
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => console.log(error))
   }
 
   return (
-    <div className="p-5 w-full">
-      <form
-        data-netlify="true"
-        name="new-Contact"
-        method="post"
+    <div className={`.text-secondary`}>
+      <Form
+        name="Contact"
         onSubmit={handleSubmit}
+        method="post"
+        action="/success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
       >
-        <input type="hidden" name="form-name" value="new-Contact" />
-        <div className="flex flex-col md:flex-row my-5 gap-5">
-          <TextField
-            id="full-name"
-            name="fullName"
-            type="text"
-            label="Full Name"
-            required
-            variant="outlined"
-            className="w-full"
-            value={fullName}
-            onChange={e => setfullName(e.target.value)}
-            sx={{
-              backgroundColor: "white",
-            }}
-          />
-          <TextField
-            id="company"
-            name="company"
-            type="text"
-            label="Company"
-            required
-            variant="outlined"
-            className="w-full"
-            value={company}
-            onChange={e => setCompany(e.target.value)}
-            sx={{
-              backgroundColor: "white",
-            }}
-          />
-        </div>
+        <input type="hidden" name="form-name" value="Contact" />
+        <Row className="ps-lg-5 pb-lg-5">
+          <Col md={6} sm={12} className="px-lg-5">
+            <Form.Group className="mb-3">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                name="firstname"
+                type="text"
+                onChange={e => setFirstName(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} sm={12} className="px-lg-5">
+            <Form.Group className="mb-3">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                name="lastname"
+                type="text"
+                onChange={e => setLastName(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="ps-lg-5  pb-lg-5">
+          <Col md={6} sm={12} className="px-lg-5">
+            <Form.Group className="mb-3">
+              <Form.Label>E-Mail</Form.Label>
+              <Form.Control
+                name="email"
+                type="email"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} sm={12} className="px-lg-5">
+            <Form.Group className="mb-3">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control
+                name="phone"
+                type="tel"
+                pattern="[0-9]{10}"
+                onChange={e => setPhone(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="ms-lg-5 mb-sm-5">
+          <Col className="px-lg-5">
+            <Form.Group className="mb-3">
+              <Form.Label>What service do you need ?</Form.Label>
+              <Row className="m-sm-3">
+                {checkboxList.map(item => (
+                  <Col xs={6} sm={4} md={3} className="mb-3 p-1">
+                    <Form.Check
+                      label={item}
+                      type="checkbox"
+                      aria-label="checkbox for following text input"
+                      name={item}
+                      checked={checkedItems.includes(item)}
+                      onChange={handleCheckboxChange}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="ms-lg-5">
+          <Col className="px-lg-5">
+            <Form.Group className="mb-md-3">
+              <Form.Label className="mb-md-5">Message</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Write your message"
+                onChange={e => setMessage(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
 
-        <div className="my-5">
-          <TextField
-            id="email"
-            name="email"
-            type="email"
-            label="Email Address"
-            required
-            variant="outlined"
-            className="w-full"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            sx={{
-              backgroundColor: "white",
-            }}
-          />
-        </div>
-
-        <div className="my-5 ">
-          <TextField
-            id="message"
-            name="message"
-            type="text"
-            label="Your Message"
-            required
-            multiline
-            rows={4}
-            className="w-full"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-            sx={{
-              backgroundColor: "white",
-            }}
-          />
-        </div>
-
-        {/* Switch */}
-        <div className="flex my-5 gap-2 items-center">
-          <Switch defaultChecked color="primary" onChange={switchHandler} />
-          <p className="text-sm">I agree to the terms and conditions</p>
-        </div>
-
-        <Button
-          disabled={!checked}
-          type="submit"
-          variant="contained"
-          className="w-full bg-[#085879] hover:bg-[#0878a8] disabled:bg-slate-400"
-        >
-          Send Message
+        <Button className={` m-sm-5 mt-3 `} type="submit">
+          Submit
         </Button>
-        <div className="flex justify-center">
-          <SocialMediaLinks />
-        </div>
-      </form>
-
-      <Subscribe />
+      </Form>
     </div>
   )
 }
